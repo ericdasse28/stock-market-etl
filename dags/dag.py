39468b185ac104ac309ce37057be799f3555d741
Eric Dasse <christ.gououi@gmail.com>
2025-01-16 09:51:01 +0100

    Feat(dag): add transform stage

--- dags/dag.py
+++ dags/dag.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 import os
 from airflow import DAG, task
 from dotenv import load_dotenv
+import pandas as pd
 import requests
 
 load_dotenv()
@@ -28,4 +29,34 @@ with DAG(
         # Return the raw data
         return response.json()
 
-    hit_polygon_api()
+    @task
+    def flatten_market_data(polygon_response, **context):
+        # Create a list of headers and a list to store
+        # the normalized data in
+        columns = {
+            "status": "closed",
+            "from": context.get("ds"),
+            "symbol": "AMZN",
+            "open": None,
+            "high": None,
+            "low": None,
+            "close": None,
+            "volume": None,
+        }
+
+        flattened_record = []
+        for header_name, default_value in columns.items():
+            flattened_record.append(
+                polygon_response.get(
+                    header_name,
+                    default_value,
+                )
+            )
+        flattened_dataframe = pd.DataFrame(
+            [flattened_record],
+            columns=columns.keys(),
+        )
+        return flattened_dataframe
+
+    raw_market_data = hit_polygon_api()
+    flatten_market_data(raw_market_data)
